81046823b586d610e60e39fa4b3763d9a5f3e067
lee93 <dohuny2@naver.com>
2024-09-09 15:43:04 +0900

    Login Access Token 발급

---
+++ src/main/java/com/lee93/coc/srcurity/JwtTokenProvider.java
@@ -0,0 +1,54 @@
+package com.lee93.coc.srcurity;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+
+    private final String SECRET_KEY = System.getenv("JWT_SECRET_KEY");
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30;
+
+    public String generateToken(String loginId) {
+        System.out.println(loginId+" ::: Generating JWT token ::: "+SECRET_KEY);
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, loginId);
+    }
+
+    private String createToken(Map<String, Object> claims, String loginId) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(loginId)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String extractUserId(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+}
+
